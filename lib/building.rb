579de06fbf8c16bcc012d1255122ebde78cd8dd2
Antonio Jackson <antoniojackson@gmail.com>
2020-04-01 09:39:47 -0600

    Adding updates from pairing session with Whitney

--- lib/building.rb
+++ lib/building.rb
@@ -31,14 +31,24 @@ class Building
   end
 
   def units_by_number_of_bedrooms
-    bedrooms_3 = @units.find_all { |unit| unit.bedrooms == 3}
-    bedrooms_2 = @units.find_all { |unit| unit.bedrooms == 2}
-    bedrooms_1 = @units.find_all { |unit| unit.bedrooms == 1}
     units_by_bedrooms = {}
-    units_by_bedrooms = { 3 => bedrooms_3.map {|bedroom_3| bedroom_3.number},
-                          2 => bedrooms_2.map {|bedroom_2| bedroom_2.number},
-                          1 => bedrooms_1.map {|bedroom_1| bedroom_1.number}}
+    units.each do |unit|
+      if units_by_bedrooms.keys.include?(unit.bedrooms)
+        units_by_bedrooms[unit.bedrooms] << unit.number
+      else
+      units_by_bedrooms[unit.bedrooms] = [unit.number]
+      end
+    end
     units_by_bedrooms
+
+    # bedrooms_3 = @units.find_all { |unit| unit.bedrooms == 3}
+    # bedrooms_2 = @units.find_all { |unit| unit.bedrooms == 2}
+    # bedrooms_1 = @units.find_all { |unit| unit.bedrooms == 1}
+    # units_by_bedrooms = {}
+    # units_by_bedrooms = { 3 => bedrooms_3.map {|bedroom_3| bedroom_3.number},
+    #                       2 => bedrooms_2.map {|bedroom_2| bedroom_2.number},
+    #                       1 => bedrooms_1.map {|bedroom_1| bedroom_1.number}}
+    # units_by_bedrooms
   end
 
   def annual_breakdown
@@ -47,5 +57,13 @@ class Building
       breakdown[rented_unit.renter.name] = rented_unit.monthly_rent * 12
     end
     breakdown
+  end
+
+  def rooms_by_renter
+    renter_rooms = {}
+    rented_units.map do |rented_unit|
+      renter_rooms[rented_unit.renter] = {bathrooms: rented_unit.bathrooms, bedrooms: rented_unit.bedrooms}
+    end
+    renter_rooms
   end
 end
